b44e8865d8543cd1972cbd339f4f1b7a8f1026d9
Lavanya Amireddy <117822121+amireddy-lavanya@users.noreply.github.com>
2023-01-05 11:23:03 +0530

    Update contacts.js

--- Models/contacts.js
+++ Models/contacts.js
@@ -1,14 +1,20 @@
-const mongoose = require("mongoose");
-const contactsSchema = new mongoose.Schema({
-    name: {type:String},
-    designation: {type:String},
-    company:{type:String},
-    industry:{type:String},
-    email: {type:String},
-    phoneNumber:{type:String},
-    country:{type:String},
-    userId: { type: String, ref: "User" }
-});
+const mongoose = require('mongoose');
+
+//defining a model
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+//create schema
+const ContactsSchema = new Schema({
+    email: {type:String,required:true,unique:true},
+    name: {type:String, required:true},
+    designation: {type:String, required:true},
+    company: {type:String, required:true},
+    industry: {type:String, required:true},
+    phone: {type:Number, required:true},
+    country: {type:String, required:true},
+    useRef:{type: String}
+})
 
 const contactsModel = new mongoose.model("contacts", contactsSchema);
 module.exports = contactsModel;
